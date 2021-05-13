526ef6a43aa13c2ddc9b92df09cbb115fd8f34c4
张磊 <zhanglei.yi@163.com>
2021-05-13 14:39:30 +0800

    删除多余代码

--- baseProject/src/main/java/com/example/template/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.template.mapper;
-
-import com.example.template.entity.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserMapper {
-
-  User findById(@Param("id") int id);
-}
--- baseProject/src/main/java/com/example/template/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.template.service;
-
-import com.example.template.entity.User;
-
-public interface UserService {
-
-  User Sel(int id);
-}
